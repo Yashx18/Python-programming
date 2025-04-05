f9b4d24c70faf363f6d083a7ed851b58c00a7fac
ken <yash20004x17@gmail.com>
2025-04-05 13:14:13 +0000

    Completed second video from Python Tutorial

--- learning.py
+++ learning.py
@@ -1,1 +1,61 @@
-print("hi")
+
+"""firstName = int(input("Enter your name here : "))
+age = 20
+userName = "ken"
+
+print(firstName, age, userName)
+
+num1 = int(input('Enter First Number:'))
+num2 = int(input('Enter Second Number:')) 
+
+print('Sum is',num1 + num2)
+
+side = float(input('Enter side of square:'))
+print('Area of square is ',side * side)
+
+num1 = float(input('Enter first number :'))
+num2 = float(input('Enter second number :'))
+
+name = 'Hello $my name $is yash $and i $am 20 years old'
+print(name.count('$'))
+
+marks = int(input('Enter your Marks :'))
+
+if(marks >= 90):
+    print('Grade A')
+elif(marks <90 and marks >= 80):
+    print('Grade B')
+elif(marks < 80 and marks >= 70):
+    print('Grade C')
+elif(marks < 70 and marks >= 60):
+    print('Grade D')
+else:
+    print('You have Failed')
+
+num = int(input("Enter number :"))
+res = num % 2
+
+if( res == 0 ):
+    print(num,'is Even')
+else:
+print((num1 + num2)/2)
+    print(num,'is Odd')
+
+
+num1 = int(input("Enter first number :"))
+num2 = int(input("Enter second number :"))
+num3 = int(input("Enter third number :"))
+
+if(num1 > num2 and num1 > num3):
+    print(num1,'is the Greatest of all numbers')
+elif(num2 > num1 and num2 > num3):
+    print(num2,'is the Greatest of all numbers')
+elif(num3 > num1 and num3 > num2):
+    print(num3,'is the Greatest of all numbers')
+"""
+
+num = int(input("Enter number :"))
+if((num % 7 ) == 0):
+    print(num,'is divisible by 7')
+else:
+    print(num,'is not divisible by 7')
